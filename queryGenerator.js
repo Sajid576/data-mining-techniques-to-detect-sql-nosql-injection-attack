08341f5b43530cb7c2a7bde54dccd4c2a5905073
Siddiqui-2583 <nurnaby83@gmail.com>
2021-09-07 21:20:41 +0600

    query generated successfully!

--- queryGenerator.js
+++ queryGenerator.js
@@ -1,31 +1,54 @@
 var randomWords = require("random-words");
-
-const queryFormat = "SELECT * FROM # WHERE  # = '#'  AND City= '#' ";
-
-let re = /#/gi;
-
-let generatedRandomQuery = queryFormat.replace(re, () => {
-  // console.log({ match });
-  return randomWords();
-});
-
-console.log(generatedRandomQuery);
-
-
+const csv = require("csv-parser");
+const fs = require("fs");
 const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 const csvWriter = createCsvWriter({
   path: "out.csv",
   header: [
     { id: "sentence", title: "Sentence" },
-    { id: "label", title: "Label" }
+    { id: "label", title: "Label" },
   ],
 });
 
+const hashQuery = []
+let generatedRandomQuery = [];
 const data = [];
-data.push({
-  sentence: generatedRandomQuery,
-  label: "0",
-});
-csvWriter
-  .writeRecords(data)
-  .then(() => console.log("The CSV file was written successfully"));
+ fs.createReadStream("hashQuery.csv")
+  .pipe(csv())
+  .on("data", (queryFormat) => {
+    // console.log(queryFormat.SQL)
+    hashQuery.push(queryFormat.SQL);
+    // console.log(hashQuery)
+  })
+   .on("end", () => {
+    //  console.log(hashQuery)
+     let re = /#/gi;
+     
+     for (let i = 0; i < hashQuery.length; i++) {
+      //  console.log(hashQuery[i]);
+       generatedRandomQuery = hashQuery[i].replace(re, () => {
+         return randomWords();
+       });
+      //  console.log(i, generatedRandomQuery);
+       data.push({
+         sentence: generatedRandomQuery,
+         label: "0",
+       });
+     }
+    //  console.log(data);
+     
+      csvWriter
+        .writeRecords(data)
+        .then(() => console.log("The CSV file was written successfully"));
+    console.log("CSV file successfully processed");
+  });
+
+
+
+
+
+
+
+
+
+
